040df1ed9fe0ee98be32b2454502fe2675fc3a06
Abhinav Anand <abhinav.a@mantralabsglobal.com>
2020-06-04 12:46:58 +0530

    bug fixes

--- api/util/email.ts
+++ api/util/email.ts
@@ -3,14 +3,14 @@ import { SmtpClient } from "https://deno.land/x/smtp/mod.ts";
 import { env } from './packages.ts';
 
 const client = new SmtpClient();
-const email = env.EMAIL;
-const email_password = env.EMAIL_PASSWORD;
+const EMAIL = env.EMAIL;
+const EMAIL_PASSWORD = env.EMAIL_PASSWORD;
 
 const params = {
     hostname: "smtp.gmail.com",
     port: 465,
-    username: email,
-    password: email_password,
+    username: EMAIL,
+    password: EMAIL_PASSWORD,
 }
   
 await client.connectTLS(params);
@@ -19,7 +19,7 @@ await client.connectTLS(params);
 export default {
     sendEmail: async (mailoption: any) => {
         return new Promise(async (resolve) => {
-            mailoption.from = email;
+            mailoption.from = EMAIL;
             try {
                 await client.send(mailoption);
                 resolve(true);
